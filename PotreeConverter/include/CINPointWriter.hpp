63590cd315480f063e9a722495a67184b45e4e4b
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-06-27 22:21:56 +0200

    Support random sorting of points within a .cin

--- PotreeConverter/include/CINPointWriter.hpp
+++ PotreeConverter/include/CINPointWriter.hpp
@@ -30,8 +30,16 @@ public:
 	AABB aabb;
 	double scale;
 
-	std::vector<float> positions;
-	std::vector<unsigned char> colors;
+	struct Position {
+		float xyz[3];
+		Position(float x, float y, float z) { xyz[0] = x; xyz[1] = y; xyz[2] = z;}
+	};
+	struct Color {
+		unsigned char rgba[4];
+		Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) { rgba[0] = r; rgba[1] = g; rgba[2] = b; rgba[3] = a;}
+	};
+	std::vector<Position> positions;
+	std::vector<Color> colors;
 
 	struct {
 		float x, y, z;
@@ -83,18 +91,10 @@ public:
 					_max.x = std::max(_max.x, x); _max.y = std::max(_max.y, y); _max.z = std::max(_max.z, z);
 				}
 
-				positions.push_back(x);
-				positions.push_back(y);
-				positions.push_back(z);
+				positions.push_back(Position(x, y, z));
 
 			}else if(attribute == PointAttribute::COLOR_PACKED){
-				// std::cout << point.intensity << '/' << (int)((unsigned char)(point.intensity)) << '/' << (int)point.color.x << std::endl;
-				// unsigned char intensity = (unsigned char)(point.intensity);// / 255);
-				unsigned char rgba[4] = {point.color.x, point.color.y, point.color.z, 255};
-				colors.push_back(rgba[0]);
-				colors.push_back(rgba[1]);
-				colors.push_back(rgba[2]);
-				colors.push_back(rgba[3]);
+				colors.push_back(Color(point.color.x, point.color.y, point.color.z, 255));
 			}else if(attribute == PointAttribute::INTENSITY){
 				// unsigned char i = (unsigned char)(255 * (point.intensity / 65535.0));
 				// colors.push_back(i);
@@ -115,85 +115,33 @@ public:
 		numPoints++;
 	}
 
-	void write2(const Point &point){
-		for(int i = 0; i < attributes.size(); i++){
-			PointAttribute attribute = attributes[i];
-			if(attribute == PointAttribute::POSITION_CARTESIAN){
-				//float pos[3] = {(float) point.x,(float)  point.y,(float)  point.z};
-				int x = (int)((point.position.x - aabb.min.x) / scale);
-				int y = (int)((point.position.y - aabb.min.y) / scale);
-				int z = (int)((point.position.z - aabb.min.z) / scale);
-				int pos[3] = {x, y, z};
-				writer->write((const char*)pos, 3*sizeof(int));
-			}else if(attribute == PointAttribute::COLOR_PACKED){
-				unsigned char rgba[4] = {point.color.x, point.color.y, point.color.z, 255};
-				writer->write((const char*)rgba, 4*sizeof(unsigned char));
-			}else if(attribute == PointAttribute::INTENSITY){
-				writer->write((const char*)&point.intensity, sizeof(unsigned short));
-			}else if(attribute == PointAttribute::CLASSIFICATION){
-				writer->write((const char*)&point.classification, sizeof(unsigned char));
-			}else if(attribute == PointAttribute::NORMAL_SPHEREMAPPED){
-				// see http://aras-p.info/texts/CompactNormalStorage.html
-				float nx = point.normal.x;
-				float ny = point.normal.y;
-				float nz = point.normal.z;
-				float lengthxy = sqrt(nx * nx + ny * ny);
-
-				float ex = 0.5f * (nx / lengthxy) * sqrt(-nz * 0.5f + 0.5f) + 0.5f;
-				float ey = 0.5f * (ny / lengthxy) * sqrt(-nz * 0.5f + 0.5f) + 0.5f;
-
-				unsigned char bx = (unsigned char)(ex * 255);
-				unsigned char by = (unsigned char)(ey * 255);
-
-				writer->write((const char*)&bx, 1);
-				writer->write((const char*)&by, 1);
-			}else if(attribute == PointAttribute::NORMAL_OCT16){
-				// see http://lgdv.cs.fau.de/get/1602
-
-				float nx = point.normal.x;
-				float ny = point.normal.y;
-				float nz = point.normal.z;
-
-				float norm1 = abs(nx) + abs(ny) + abs(nz);
-
-				nx = nx / norm1;
-				ny = ny / norm1;
-				nz = nz / norm1;
-
-				float u = 0;
-				float v = 0;
-
-				if(nz >= 0){
-					u = nx;
-					v = ny;
-				}else{
-					u = psign(nx)*(1-psign(ny)*ny);
-					v = psign(ny)*(1-psign(nx)*nx);
-				}
-
-				unsigned char bx = (unsigned char)(min((u + 1) * 128, 255.0f));
-				unsigned char by = (unsigned char)(min((v + 1) * 128, 255.0f));
-
-				writer->write((const char*)&bx, 1);
-				writer->write((const char*)&by, 1);
-			}else if(attribute == PointAttribute::NORMAL){
-				writer->write((const char*)&point.normal.x, sizeof(float));
-				writer->write((const char*)&point.normal.y, sizeof(float));
-				writer->write((const char*)&point.normal.z, sizeof(float));
-			}
-		}
-
-		numPoints++;
-	}
 
 	void close(){
 		if(writer != NULL){
-			// std::cout << "WRITING FOR REAL " << file << ':' << numPoints<<  ',' << (positions.size() / 3) << ',' << (colors.size() / 4) << std::endl;
-
 			writer->write((const char*)&_min, 3 * sizeof(int));
 			writer->write((const char*)&_max, 3 * sizeof(int));
-			writer->write((const char*)positions.data(), positions.size() * sizeof(int));
-			writer->write((const char*)colors.data(), colors.size() * sizeof(unsigned char));
+
+			// randomize
+			if (true) {
+				// ...
+				std::vector<size_t> indices(positions.size());
+				const size_t count = positions.size();
+				for (size_t i=0; i<count; i++) {
+					indices[i] = i;
+				}
+				std::random_shuffle(indices.begin(), indices.end());
+
+				for (size_t i=0; i<count; i++) {
+					writer->write((const char*)&(positions[indices[i]].xyz), 3 * sizeof(float));
+				}
+				for (size_t i=0; i<count; i++) {
+					writer->write((const char*)&(colors[indices[i]].rgba), 4);
+				}
+			} else {
+				writer->write((const char*)positions.data(), positions.size() * 3 * sizeof(int));
+            	writer->write((const char*)colors.data(), colors.size() * 4 * sizeof(unsigned char));
+			}
+
 
 
 			writer->close();
